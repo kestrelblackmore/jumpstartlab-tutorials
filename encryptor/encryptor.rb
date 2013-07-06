11bca300cb2890069562ce80c0b59c8d263c6c5b
Kestrel Blackmore <me@kestrelblackmore.com>
2013-07-06 16:20:32 +0930

    added file encryption/decryption

--- encryptor/encryptor.rb
+++ encryptor/encryptor.rb
@@ -1,34 +1,62 @@
 class Encryptor
 
-  def decrypt(string)
-    encrypt(string)
+  def decrypt_file(filename, rotation)
+    new_filename = filename.gsub("encrypted", "decrypted")
+
+    decrypted_text = decrypt(get_file_contents(filename), rotation)
+
+    write_to_file(new_filename, decrypted_text)
+
+    puts "Decrypted #{filename} to #{new_filename}."
   end
 
-  def encrypt(string)
+  def encrypt_file(filename, rotation)
+    new_filename = "#{filename}.encrypted"
 
+    encrypted_text = encrypt(get_file_contents(filename), rotation)
+
+    write_to_file(new_filename, encrypted_text)
+
+    puts "Encrypted #{filename} to #{new_filename}."
+  end
+
+  def write_to_file(filename, data)
+    output = File.open(filename, "w")
+    output.write(data)
+    output.close
+  end
+
+  def get_file_contents(filename)
+    input = File.open(filename, "r")
+    contents = input.read
+    input.close
+    return contents
+  end
+
+  def decrypt(string, rotation)
+    encrypt(string, rotation * -1)
+  end
+
+  def encrypt(string, rotation)
     # do encryption
     results = string.split("").collect do |letter|
-      encrypt_letter(letter)
+      encrypt_letter(letter, rotation)
     end
 
     results.join
-
   end
 
-  def encrypt_letter(letter)
-    cipher[letter.downcase]
+  def encrypt_letter(letter, rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
   end
 
-  def cipher
-    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
-     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-     'y' => 'l', 'z' => 'm'}
+  def cipher(rotation)
+    characters = (' '..'z').to_a
+    rotated_characters = characters.rotate(rotation)
+    
+    # zip joins two arrays together. Hash turns them into a true key/value pair
+    Hash[characters.zip(rotated_characters)]
   end
-
-
 
 end
